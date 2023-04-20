7e349d4240b1286ed644f6f6ddee0ece5f14aa2c
Sarah Ahmed <sarah.ahmed9765@gmail.com>
2023-04-20 07:25:30 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unisted.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdount);
+	return (1);
+}
