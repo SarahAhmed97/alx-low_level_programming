515af69312dc5f005b3d9caa288dc24830ee95ad
Sarah Ahmed <sarah.ahmed9765@gmail.com>
2023-04-20 06:37:24 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  *this is a c code that prints exactly "Programming is like building a multilingual puzzle, followed by a new line."
- * >return o
- * */
+ * return o
+ */
 int main (void)
-{puts("\"Programming is like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
