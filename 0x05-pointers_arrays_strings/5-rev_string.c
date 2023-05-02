45320d2383774a20860b8a5aa38b8962856262c0
Sarah Ahmed <sarah.ahmed9765@gmail.com>
2023-05-02 14:41:12 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,10 @@ void rev_string(char *s)
 	int l, i;
 	char temp;
 
-	for (l = 0; s[1] != '\0'; ++l)
-		;
+	while (s[l++])
+		l++;
 
-	for (i = 0; i < l / 2; i++)
+	for (i = l - 1; i >= l / 2; i--)
 	{
 		temp = s[i];
 		s[i] = s[l - 1 - i];
