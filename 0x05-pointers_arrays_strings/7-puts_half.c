43efc9aefc7fb5bb1bcb3e16f22ff6e0dc5281e7
Sarah Ahmed <sarah.ahmed9765@gmail.com>
2023-05-02 14:14:34 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,17 +14,8 @@ void puts_half(char *str)
 	for (i = 0; str[i] != '\0'; ++i)
 		;
 
-	if (i % 2 == 0)
-	{
-		for (i /= 2; str[i] != '0'; i++)
-			_putchar(str[i]);
-	}
-
-	if (i % 2 == 1)
-	{
-		for ((i + 1) /= 2; str[i] != '0'; i++)
-			_putchar(str[i]);
-	}
+	for (i /= 2; str[i] != '0'; i++)
+		_putchar(str[i]);
 
 	_putchar('\n');
 }
