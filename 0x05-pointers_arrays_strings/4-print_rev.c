b6f2ad859e0fbd4349aa0265afb0ee244aa3b944
Sarah Ahmed <sarah.ahmed9765@gmail.com>
2023-05-02 11:50:55 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string in reverse
+ * @str: the string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int counter = 0;
+
+	while (s[counter] != '\0')
+		counter++;
+
+	for (counter -= 1; counter >= 0; counter--)
+		_putchar(s[c]);
+}
