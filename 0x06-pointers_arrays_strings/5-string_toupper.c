cf6c39917e2260979bd8405e67324a0db362a74f
Sarah Ahmed <sarah.ahmed9765@gmail.com>
2023-05-04 04:53:41 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - changes all lowercase letters
+ * to uppercase
+ * @str: string to be changed
+ * Return: pointer to the changed string
+ */
+
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+			i++;
+		}
+		return (str);
+	}
+}
