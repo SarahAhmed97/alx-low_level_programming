77bb39bed84c732998b4edd13236e3f702a8ab30
Sarah Ahmed <sarah.ahmed9765@gmail.com>
2023-05-12 09:34:47 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: int
+ * @argv: int
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("error\n');
+		return (1);
+	}
+	return (0);
+}
