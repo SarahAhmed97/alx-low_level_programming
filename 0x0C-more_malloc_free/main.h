c183de12de6b1cb947bd314d26e465f433e24c60
Sarah Ahmed <sarah.ahmed9765@gmail.com>
2023-05-18 14:00:19 -0700

    task 5

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,9 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memset(char *s, char b, unsigned int n);
+void _puts(char *str)
+int _atoi(const char *s)
+void print_int(unsigned long int n)
+int main(int argc, char const *argv[])
 
 #endif
