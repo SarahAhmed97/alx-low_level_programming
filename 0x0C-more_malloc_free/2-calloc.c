d84a8bfa9f212ed3607c8f9dc5211e9a39a02151
Sarah Ahmed <sarah.ahmed9765@gmail.com>
2023-05-18 14:08:25 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,45 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * _calloc - allocates memory for an array, using malloc
- * @nmemb: int
- * @size: int
- * Return: 0
+ * _calloc - allocates memory for an array of @nmemb elements of
+ * @size bytes each and returns a pointer to the allocated memory.
+ *
+ * @nmemb: allocate memory for array
+ * @size: allocate element of size bytes
+ *
+ * Return: pointer to the allocated memory.
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *m;
+	char *a;
+	unsigned int b;
 
-	if (size == 0 || nmemb == 0)
-		return (0);
-
-	m = malloc(sizeof(int) * nmemb);
-
-	if (m == 0)
-		return (0);
-
-	_memset(m, 0, sizeof(int) * nmemb);
-
-	return (m);
-}
-
-/**
- * _memset - fills memory with a constant byte
- * @s: int
- * @b: int
- * @n: int
- * Reurn: 0
- */
-
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *ptr = s;
-
-	while (n--)
-		*s++ = b;
-
-	return (ptr);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a = malloc(nmemb * size);
+	if (a == NULL)
+		return (NULL);
+	for (b = 0; b < (nmemb * size); b++)
+		a[b] = 0;
+	return (a);
 }
