0795005962a0ad2e94c9e1b6dbd65bbe69fc32b0
Sarah Ahmed <sarah.ahmed9765@gmail.com>
2023-05-22 08:09:16 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: Owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
