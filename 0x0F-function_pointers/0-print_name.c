3220b8188d56888e98949fe1c23a0f6da8232d26
Sarah Ahmed <sarah.ahmed9765@gmail.com>
2023-05-24 06:18:21 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: string
+ * @f: function pointer
+ *
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
