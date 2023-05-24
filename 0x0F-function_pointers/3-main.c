bd8947f9221e587710c57e714eea4974e5f979e4
Sarah Ahmed <sarah.ahmed9765@gmail.com>
2023-05-24 11:23:10 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,24 +10,24 @@
  * Return: 0
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int (*op_func)(int, int), a, b;
+	int (*oprt)(int, int);
 
 	if (argc != 4)
-		printf("Error\n"), exit(98);
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+	oprt = get_op_func(argv[2]);
 
-	op_func = get_op_func(argv[2]);
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-	if (!op_func)
-		printf("Error\n"), exit(99);
-
-	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-		printf("Error\n"), exit(100);
-
-	printf("%d\n", op_func(a, b));
+	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
