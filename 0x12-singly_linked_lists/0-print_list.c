0b5044eae21d0778dd7a9f5f4b2daa381dc05d2a
Sarah Ahmed <sarah.ahmed9765@gmail.com@gmail.com>
2023-06-01 04:42:13 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: int length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * size_t print_list - prints all the elements of a list_t list
+ * @h: pointer to 1st node
+ *
+ * Return: int size of list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
