0a635158ec17c60aedd237851793933c8132976a
Sarah Ahmed <sarah.ahmed9765@gmail.com@gmail.com>
2023-06-01 05:03:04 -0700

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node - add a new node at the beginning of a list_t list
+ * @head: pointer's address to head node
+ * @st: str field of node
+ *
+ * Return: size of int
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_head = malloc(sizeof(list_t));
+
+	if (!head || !new_head)
+		return (NULL);
+	if (str)
+	{
+		new_head->str = strdup(str);
+
+		if (!new_head->str)
+		{
+			free(new_head);
+			return (NULL);
+		}
+		new_head->len = _strlen(new_head->str);
+	}
+	new_head->next = *head;
+	*head = new_head;
+	return (new_head);
+}
+
