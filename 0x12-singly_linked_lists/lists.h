aeb9618b4bed2f1edbf3a729fc583b2960d1ac7c
Sarah Ahmed <sarah.ahmed9765@gmail.com@gmail.com>
2023-06-01 04:48:51 -0700

    task 1

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,14 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-int _putchar(char c);
-int _strken(char *s);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
 /**
  * STRUCT LISTS_S - SINGLY LINKED LIST
  * @STR: STRING - (MALLOC STRING)
@@ -27,5 +19,13 @@ typedef struct lists_s
 	unsigned int len;
 	struct lists_s *next;
 } lists_t;
+
+int _putchar(char c);
+int _strken(char *s);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
