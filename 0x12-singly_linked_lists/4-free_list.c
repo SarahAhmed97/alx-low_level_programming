c8863e7bcdeaa7a9502c1fae817b60d78808b600
Sarah Ahmed <sarah.ahmed9765@gmail.com@gmail.com>
2023-06-01 05:22:54 -0700

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_list -  frees a list_t list.
+ * @head: head node's pointer
+ *
+ * Return: 0
+ */
+void free_list(list_t *head)
+{
+	list_t *node, *next_node;
+
+	if (!head)
+		return;
+
+	node = head;
+
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
+}
