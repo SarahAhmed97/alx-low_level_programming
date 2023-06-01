bc257eeb850c9928d7112a5d77b52eead0c5d036
Sarah Ahmed <sarah.ahmed9765@gmail.com@gmail.com>
2023-06-01 11:45:57 -0700

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,14 +13,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *node = *head;
 
 	if (!head || !new_node)
-		return (0);
+		return (NULL);
 	if (str)
 	{
 		new_node->str = strdup(str);
 		if (!new_node->str)
 		{
 			free(new_node);
-			return (0);
+			return (NULL);
 		}
 		new_node->len = _strlen(new_node->str);
 	}
