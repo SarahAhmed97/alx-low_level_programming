0ae0a9104bd4f6ecd51c873167f3b627a0147581
Sarah Ahmed <sarah.ahmed9765@gmail.com@gmail.com>
2023-06-04 22:41:13 -0700

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint -  deletes the head node of a listint_t linked list
+ * @head: pointer to 1st node
+ * Return: head node’s data (n) or NULL.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *node;
+	int n;
+
+	if (!head || !*head)
+		return (0);
+
+	node = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = node;
+	return (n);
+}
