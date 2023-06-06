fe930886a26a4fa50b4a0fa69f2d6f3964fe7c43
Sarah Ahmed <sarah.ahmed9765@gmail.com@gmail.com>
2023-06-06 13:23:09 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,17 +8,14 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *node, temp;
+	listint_t *next;
 
-	if (!head)
+	if (head == NULL)
 		return;
-
-	node = *head;
-	while (node)
+	while (*head != NULL)
 	{
-		temp = node;
-		node = node->next;
-		free(temp);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
-	*head = NULL;
 }
