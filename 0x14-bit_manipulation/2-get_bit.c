dbbc6d778eb2b24a66be55466e31195104d2de92
Sarah Ahmed <sarah.ahmed9765@gmail.com@gmail.com>
2023-06-08 06:58:43 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,10 +7,10 @@
  * Return: 0
  */
 
-int get_bit(unsigned long int n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
 
-	return (n >> index & 1);
+	ireturn (n >> index & 1);
 }
