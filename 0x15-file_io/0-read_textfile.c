a62a6c12054f0aa4445839afa63b5cfb362a3e68
Sarah Ahmed <sarah.ahmed9765@gmail.com@gmail.com>
2023-06-12 05:03:04 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,6 +19,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	bytes = read(fd, &buf[0], letters);
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
-	clode(fd);
+	close(fd);
 	return (bytes);
 }
